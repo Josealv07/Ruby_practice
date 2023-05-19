689db8b3604006f23cce6e0aa89e6cf9e2092e01
Jose AlVarez <Jose07alvarezm@gmail.com>
2023-05-18 22:14:05 -0500

    Little prove of basics at ruby

--- pruebas.rb
+++ pruebas.rb
@@ -1,4 +1,20 @@
 #condicionales
+x = "Hi guys i'm doing this as a prove of my work studing Ruby on rails :)"
+puts x
+
+puts "I know how define a variable but ruby uses all like objects, look the next example: "
+puts x.class
+
+puts "You can also define an array just like you do it in python :)"
+teammates = ["Juliana", "Juan Diaz", "Olmeiro", "Daniel F", "Jose"]
+
+
+puts teammates.map { |x| "Hi #{x}"}
+
+roles = {}
+for i in teammates do
+    puts "You are #{i}"
+end
 
 is_authenticated = true
 
